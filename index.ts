251ea2eec63a2b46a552f568f44816e9fdf13b2c
Devansh Bhavsar <devanshbhavsar2006@gmai.com>
2026-01-30 14:28:50 +0530

    init

---
+++ index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import twilio from "twilio";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const accountSid = process.env.ACCOUNTSID;
+const authToken = process.env.AUTHTOKEN;
+const client = twilio(accountSid, authToken);
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/call", async (req, res) => {
+  const body = req.body;
+
+  try {
+    const res = await client.calls.create({
+      to: body.phoneNo,
+      from: "+16403568498",
+      twiml: `<Response><Say>There is an accident at ${body.location}. Please reach there quickly.</Say></Response>`,
+    });
+
+    console.log(res);
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+app.listen(3001);
